c5fc2e88e60507014b2f50e8be4948db47a813b1
Omkar Ghaisas <oaghaisas@gmail.com>
2017-07-13 17:55:03 +0530

    Multiple Changes
    Member Mobile number set to 10 digit
    Added Member email validation
    Added address selection and display

--- library_management/library_management/doctype/library_member/library_member.js
+++ library_management/library_management/doctype/library_member/library_member.js
@@ -2,7 +2,27 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Library Member', {
-	refresh: function(frm) {
-
+	email: function(frm) {
+		var is_email = frappe.utils.validate_type(frm.doc.email,"email");
+		if(!is_email){
+			frm.set_value("email","");
+			frappe.msgprint(__("Please enter valid email"));
+		}
+	},
+	address: (frm) => {
+		var me = this;
+		if(this.frm.doc.address) {
+			frappe.call({
+				method: "frappe.contacts.doctype.address.address.get_address_display",
+				args: {"address_dict": this.frm.doc.address },
+				callback: function(r) {
+					if(r.message) {
+						me.frm.set_value("address_display", r.message)
+					}
+				}
+			})
+		} else {
+			this.frm.set_value("address_display", "");
+		}
 	}
 });
